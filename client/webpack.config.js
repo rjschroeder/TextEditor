3558d1070424564f3573ab553fb25ac4311c26d3
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-05 18:00:20 -0600

    passed in parameter object for htmlwebpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,7 +18,10 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      new HtmlWebpackPlugin()
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "JATE"
+      })
     ],
 
     module: {
