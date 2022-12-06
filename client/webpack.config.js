8718fb2254a89f0c83e61c32cb035700f848ad8c
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-05 18:05:52 -0600

    configured service worker and manifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -21,6 +21,28 @@ module.exports = () => {
       new HtmlWebpackPlugin({
         template: "./index.html",
         title: "JATE"
+      }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Just Another Text Editor",
+        short_name: "Text Editor",
+        description: "One page PWA text editor",
+        background_color: '#22a372',
+        theme_color: '#22a372',
+        start_url: "./",
+        publicPath: "./",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          }
+        ]
       })
     ],
 
